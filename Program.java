1fc341315a5fd2f8f7658175ce6e0739d80dbe16
student <student@lab-44-7.cs.put.poznan.pl>
2018-10-30 10:37:40 +0100

    zmiana 2

--- Program.java
+++ Program.java
@@ -5,7 +5,8 @@ void branch(){
    /* This is my first java program.
     * This will print 'Hello World' as the output
     */
-
+string hello(){
+}
    public static void main(String []args) {
       System.out.println("Hello World");
    }
